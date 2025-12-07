3275c7881fe8f27a0f8f647bf9a193bdb3ecc46a
Amit Eliya <amiteliya19@gmail.com>
2025-12-07 19:10:32 +0200

    Refactor navigation: Add Sidebar, update Layout, fix App syntax

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="site-header">
+            <div className="header-content">
+                <h1 className="site-title">WORKOUT TRACKER</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
